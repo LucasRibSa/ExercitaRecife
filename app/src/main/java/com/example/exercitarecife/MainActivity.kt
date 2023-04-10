208bcd96321de0235e7d49df55b667b2f447a543
Lucas Ribeiro <lucasribsa@icloud.com>
2023-04-09 23:10:16 -0300

    mudancas no codigo

--- app/src/main/java/com/example/exercitarecife/MainActivity.kt
+++ app/src/main/java/com/example/exercitarecife/MainActivity.kt
@@ -1,11 +1,10 @@
 package com.example.exercitarecife
-
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
-import android.widget.EditText
+
+
 
 class MainActivity : AppCompatActivity() {
     class MainActivity : AppCompatActivity() {
@@ -14,24 +13,19 @@ class MainActivity : AppCompatActivity() {
             super.onCreate(savedInstanceState)
             setContentView(R.layout.activity_main)
 
-            val editTextUsername = findViewById<EditText>(R.id.editTextUsername)
-            val editTextPassword = findViewById<EditText>(R.id.editTextPassword)
+            val buttonRegister = findViewById<Button>(R.id.buttonRegister)
             val buttonLogin = findViewById<Button>(R.id.buttonLogin)
 
             buttonLogin.setOnClickListener {
-                val username = editTextUsername.text.toString()
-                val password = editTextPassword.text.toString()
                 val intent = Intent(this, MenuActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+            buttonRegister.setOnClickListener {
+                val intent = Intent(this, RegisterActivity::class.java)
                 startActivity(intent)
                 finish()
             }
         }
     }
-
-    fun registerUser(view: View) {
-        val intent = Intent(this, RegisterActivity::class.java)
-        startActivity(intent)
-    }
-
-
 }

a43ac1bdf36ab464f9990e4ea7763485238b3b4a
Lucas Ribeiro <lucasribsa@icloud.com>
2023-06-01 14:05:21 -0300

    banco de dados implementado na aplicacao

---
+++ app/src/main/java/com/example/exercitarecife/Dados.kt
@@ -0,0 +1,7 @@
+package com.example.exercitarecife
+
+data class Dados(
+    var reg: String? = null,
+    var usuario: String? = null,
+    var senha: String? = null
+)
--- app/src/main/java/com/example/exercitarecife/RegisterActivity.kt
+++ app/src/main/java/com/example/exercitarecife/RegisterActivity.kt
@@ -2,21 +2,52 @@ package com.example.exercitarecife
 import android.content.Intent
 import com.example.exercitarecife.databinding.ActivityRegisterBinding
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class RegisterActivity : AppCompatActivity() {
     private lateinit var binding: ActivityRegisterBinding
+    private lateinit var dbRef: DatabaseReference
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(binding.root)
         val registerButton = binding.registerButton
         val backToLoginButton = binding.backToLoginButton
+        val username = binding.usernameEditText
+        val password = binding.passwordEditText
+        dbRef = FirebaseDatabase.getInstance().getReference("Registro")
 
         registerButton.setOnClickListener {
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
+            val usuario = username.text.toString()
+            val senha = password.text.toString()
+            if (usuario.isEmpty()){
+                username.error = "por favor, digite um nome de usuario"
+            }
+            if (senha.isEmpty()) {
+                password.error = "por favor, digite uma senha"
+            }
+            val reg = dbRef.push().key!!
+            val registro = Dados(reg, usuario, senha)
+
+            dbRef.child(reg).setValue(registro)
+                .addOnCompleteListener{
+                    Toast.makeText(this, "Cadastro realizado", Toast.LENGTH_SHORT).show()
+
+                    username.text?.clear()
+                    password.text?.clear()
+
+
+                }.addOnFailureListener{err ->
+                    Toast.makeText(this, "Error ${err.message}", Toast.LENGTH_SHORT).show()
+                }
+            //val intent = Intent(this, MainActivity::class.java)
+            //startActivity(intent)
         }
+
+
 
         backToLoginButton.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
